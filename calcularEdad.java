6fec0594ecefa5059a1185e7ea67f5e4ab8fe733
ae200616 <ae200616@gmail.com>
2019-07-14 22:40:04 -0500

    Create calcularEdad.java
    El programa es simple, al ejecutarse solicita que se ingrese por teclado el año, después el mes y después el día de nacimiento, los almacena por separado en variables locales tipo entero, estas variables son enviadas a la función calcularEdad, en esta con la ayuda de librería Calendar, obtengo la fecha actual y le resto el año, mes y días ingresados, confirmo que la diferencia de los meses no sea negativa o que los días sean negativos caso contrario resto un año, ya que aun no se cumplido un año más, devuelvo el valor de la edad, creo un archivo para ser escrito, grabo la edad en el archivo y cierro el archivo, y la aplicación se cierra.

---
+++ calcularEdad.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Calendar;
+
+
+ 
+public class calcularEdad {
+ 
+	public static void main(String[] args) throws IOException {
+		FileWriter flwriter = null;
+                int miaño;
+                int mimes;
+                int midia;
+                int edad;
+                Scanner teclado = new Scanner(System.in);
+                System.out.print("Introduzca su año de nacimiento: ");
+                miaño = teclado.nextInt();
+                System.out.print("Introduzca su mes de nacimiento: ");
+                mimes = teclado.nextInt();
+                System.out.print("Introduzca su día de nacimiento: ");
+                midia = teclado.nextInt();
+		edad = calcularEdad(miaño,mimes,midia);
+                flwriter = new FileWriter("C:\\Cursos\\archivo.txt");
+                flwriter.write(Integer.toString(edad));
+                flwriter.close();
+                        
+	}
+ 
+    public static int calcularEdad(int a, int m, int d) {
+        Calendar fechaActual = Calendar.getInstance();
+        int años = fechaActual.get(Calendar.YEAR) - a;
+        int meses = fechaActual.get(Calendar.MONTH) - m;
+        int dias = fechaActual.get(Calendar.DAY_OF_MONTH) - d;
+ 
+        
+        if(meses < 0 // Aún no es el mes de su cumpleaños
+           || (meses==0 && dias < 0)) { // o es el mes pero no ha llegado el día.
+            años--;
+        }
+        return años;
+    }
+}
